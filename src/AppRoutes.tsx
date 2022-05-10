52ddc528c2c59429eea43edb4466b847ef169f24
min0k <mino.kim@minokim.com>
2022-05-09 22:18:53 -0700

    Organize file structure

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,21 +1,84 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Free } from "./Free";
-
-import { Home } from "./Home";
-import { Settings } from "./Settings";
+import { Free } from "./pages/Free";
+import { Home } from "./pages/Home";
+import { Settings } from "./pages/Settings";
 import { About } from "./About";
+import { useState } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
 
 export const AppRoutes = () => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/free" element={<Free />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <AppShell
+        styles={{
+          main: {
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        }}
+        navbarOffsetBreakpoint="sm"
+        asideOffsetBreakpoint="sm"
+        fixed
+        navbar={
+          <Navbar
+            p="md"
+            hiddenBreakpoint="sm"
+            hidden={!opened}
+            width={{ sm: 200, lg: 300 }}
+          >
+            <Text>Application navbar</Text>
+          </Navbar>
+        }
+        footer={
+          <Footer height={60} p="md">
+            <Text align="center" size="sm">
+              {" "}
+              Made by Mino Kim
+            </Text>
+          </Footer>
+        }
+        header={
+          <Header height={70} p="md">
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+              <Text>Application header</Text>
+            </div>
+          </Header>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/free" element={<Free />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </AppShell>
     </BrowserRouter>
   );
 };
---
+++ src/components/LightDarkModeButton.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LightDarkModeButton = () => {
+  return <div>LightDarkModeButton</div>;
+};

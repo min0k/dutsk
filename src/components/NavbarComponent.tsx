f4fdedbf444f6daf825888024f256a56151dc505
min0k <mino.kim@minokim.com>
2022-05-10 20:54:29 -0700

    Refactor navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,17 +1,12 @@
 import { MainLink } from "./MainLinks";
-import { FiHome, FiCircle, FiSettings, FiBookOpen } from "react-icons/fi";
+import { navbarData } from "../data/navbarData";
 
 export const NavbarComponent = () => {
-  const navbarData = [
-    { icon: <FiHome size={16} />, color: "yellow", label: "Home" },
-    { icon: <FiCircle size={16} />, color: "cyan", label: "Free" },
-    { icon: <FiSettings size={16} />, color: "orange", label: "Settings" },
-    { icon: <FiBookOpen size={16} />, color: "green", label: "About" },
-  ];
-
-  const links = navbarData.map((link) => (
-    <MainLink {...link} key={link.label} />
-  ));
-
-  return <div>{links}</div>;
+  return (
+    <div>
+      {navbarData.map((link) => (
+        <MainLink {...link} key={link.label} />
+      ))}
+    </div>
+  );
 };

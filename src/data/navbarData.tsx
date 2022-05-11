a5d9e515d6e3f8508a3ef178ac566ee2f8d5f6ab
min0k <mino.kim@minokim.com>
2022-05-10 20:51:51 -0700

    Refactor data into own files/folder

---
+++ src/data/navbarData.tsx
@@ -0,0 +1,8 @@
+import { FiHome, FiCircle, FiSettings, FiBookOpen } from "react-icons/fi";
+
+export const navbarData = [
+  { icon: <FiHome size={16} />, color: "yellow", label: "Home" },
+  { icon: <FiCircle size={16} />, color: "cyan", label: "Free" },
+  { icon: <FiSettings size={16} />, color: "orange", label: "Settings" },
+  { icon: <FiBookOpen size={16} />, color: "green", label: "About" },
+];
---
+++ src/data/questionSetData.tsx
@@ -0,0 +1,19 @@
+import shib1 from "../assets/shib1.png";
+import shib2 from "../assets/shib2.png";
+import shib3 from "../assets/shib3.png";
+import shib4 from "../assets/shib4.png";
+import shib5 from "../assets/shib5.png";
+import shib6 from "../assets/shib6.png";
+
+export const questionSetData = [
+  {
+    title: "Anxiety",
+    description: "Description 1",
+    image: shib1,
+  },
+  { title: "Rejection", description: "Description 2", image: shib2 },
+  { title: "Anger", description: "Description 3", image: shib3 },
+  { title: "Happiness", description: "Description 4", image: shib4 },
+  { title: "Procrastination", description: "Description 5", image: shib5 },
+  { title: "Doubt", description: "Description 6", image: shib6 },
+];

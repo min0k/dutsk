97534a37169245f451fb3d8d71a151e8ea077fff
min0k <mino.kim@minokim.com>
2022-05-10 21:46:45 -0700

    Add quote

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,7 +1,6 @@
-import { Anchor, Box, Mark, Text, Title, useMantineTheme } from "@mantine/core";
+import { Anchor, Blockquote, Box, Group, Text, Title } from "@mantine/core";
 
 export const HomeHeader = () => {
-  const theme = useMantineTheme();
   return (
     <div>
       {" "}
@@ -11,14 +10,24 @@ export const HomeHeader = () => {
           borderRadius: theme.radius.md,
         })}
       >
-        <Title mb="lg">What is this?</Title>
-        <Text mb="lg">
-          These are a set of questions prepared by{" "}
-          <Anchor href="https://mantine.dev/" target="_blank">
-            John Kim, Ph.D.
+        <Text size="lg" pb="xl" pl="xl">
+          [Short message from person who prepared the questions.]
+        </Text>
+        <Text size="lg" pb="sm" pl="xl" variant="text">
+          These questions were prepared by{" "}
+          <Anchor weight="bold" href="https://mantine.dev/" target="_blank">
+            [Person]
           </Anchor>
         </Text>
-        <Text>Pick one and start writing, nothing is saved.</Text>
+
+        <Blockquote
+          pb="xl"
+          color="blue"
+          cite="– Haruki Murakami, Norwegian Wood"
+          sx={{ fontSize: "small" }}
+        >
+          What happens when people open their hearts? They get better.
+        </Blockquote>
       </Box>
     </div>
   );

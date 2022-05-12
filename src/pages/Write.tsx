c7c2de948971deb6d5959d69544a8de239d3e2dd
min0k <mino.kim@minokim.com>
2022-05-11 22:18:39 -0700

    Create answer state

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,24 +1,24 @@
 import { Button, Container, Text, Textarea, Title } from "@mantine/core";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
-
-interface QuestionState {
-  questions: string[];
-}
+import { IQuestionState } from "../ts/Interfaces";
 
 export const Write = () => {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [textValue, setTextValue] = useState<string>("");
   const [complete, setComplete] = useState<boolean>(false);
+  const [answers, setAnswers] = useState<string[]>([]);
 
   const location = useLocation();
-  const questions = location.state as QuestionState;
+  const questions = location.state as IQuestionState;
 
   const handleButtonClick = () => {
     if (currentQuestion === questions.questions.length - 1) {
+      setAnswers((prevAnswers) => [...prevAnswers, textValue]);
       setComplete(true);
     } else {
       setCurrentQuestion((prevCurrentQuestion) => prevCurrentQuestion + 1);
+      setAnswers((prevAnswers) => [...prevAnswers, textValue]);
       setTextValue("");
     }
   };
@@ -32,7 +32,13 @@ export const Write = () => {
   });
 
   if (complete) {
-    return <Title>Complete</Title>;
+    return (
+      <Title>
+        {answers.map((answer, idx) => (
+          <Text key={idx}>{answer}</Text>
+        ))}
+      </Title>
+    );
   }
 
   return (

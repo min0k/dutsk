8206267e7d86c52b10b4b7ddc87ec95b3d999d1a
min0k <mino.kim@minokim.com>
2022-05-11 22:37:31 -0700

    Fix typo

--- src/data/questionCardData.tsx
+++ src/data/questionCardData.tsx
@@ -13,7 +13,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
   {
@@ -23,7 +23,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
   {
@@ -33,7 +33,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
   {
@@ -43,7 +43,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
   {
@@ -53,7 +53,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
   {
@@ -63,7 +63,7 @@ export const questionCardData = [
     questions: [
       "What is something you wish you had?",
       "Why do you want it?",
-      "What would happen if you never got it?",
+      "What would happen if you never get it?",
     ],
   },
 ];

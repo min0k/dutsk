f028133a78b64ff6829a1c3ff3b68a048cbe196c
min0k <mino.kim@minokim.com>
2022-05-12 17:46:12 -0700

    Add logout button to navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,12 +1,26 @@
 import { MainLink } from "./MainLinks";
-import { navbarData } from "../data/navbarData";
+import { navbarLinks, navbarSettingsLogout } from "../data/navbarData";
 
 export const NavbarComponent = () => {
   return (
-    <div>
-      {navbarData.map((link) => (
-        <MainLink {...link} key={link.label} />
-      ))}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
+      <div>
+        {navbarLinks.map((link) => (
+          <MainLink {...link} key={link.label} />
+        ))}
+      </div>
+      <div>
+        {navbarSettingsLogout.map((link) => (
+          <MainLink {...link} key={link.label} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/data/navbarData.tsx
+++ src/data/navbarData.tsx
@@ -1,8 +1,21 @@
-import { FiHome, FiCircle, FiSettings, FiBookOpen } from "react-icons/fi";
+import {
+  FiHome,
+  FiCircle,
+  FiSettings,
+  FiBookOpen,
+  FiLogIn,
+} from "react-icons/fi";
 
-export const navbarData = [
-  { icon: <FiHome size={16} />, color: "yellow", label: "Home" },
-  { icon: <FiCircle size={16} />, color: "cyan", label: "Free" },
-  { icon: <FiSettings size={16} />, color: "orange", label: "Settings" },
-  { icon: <FiBookOpen size={16} />, color: "green", label: "About" },
+const ICON_SIZE = 16;
+
+export const navbarLinks = [
+  { icon: <FiHome size={ICON_SIZE} />, color: "yellow", label: "Home" },
+  { icon: <FiCircle size={ICON_SIZE} />, color: "cyan", label: "Free" },
+
+  { icon: <FiBookOpen size={ICON_SIZE} />, color: "green", label: "About" },
+];
+
+export const navbarSettingsLogout = [
+  { icon: <FiSettings size={ICON_SIZE} />, color: "orange", label: "Settings" },
+  { icon: <FiLogIn size={ICON_SIZE} />, color: "Indigo", label: "Login" },
 ];

2cccec22ba444a30221f3185dcf6e0e68d08237b
min0k <mino.kim@minokim.com>
2022-05-15 17:12:54 -0700

    Remove console.log

--- src/auth0Provider.js
+++ src/auth0Provider.js
@@ -4,8 +4,6 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-  console.log(window.location.origin);
-
   return (
     <Auth0Provider
       domain={domain}

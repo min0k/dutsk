c08f1b2468fa9cad90df13dceab84db3c541cace
min0k <mino.kim@minokim.com>
2022-05-15 17:10:06 -0700

    Remove Login/Logout data

--- src/data/navbarData.tsx
+++ src/data/navbarData.tsx
@@ -1,20 +1,10 @@
-import {
-  FiHome,
-  FiCircle,
-  FiSettings,
-  FiBookOpen,
-  FiLogIn,
-} from "react-icons/fi";
+import { FiHome, FiCircle, FiSettings, FiBookOpen } from "react-icons/fi";
 
-const ICON_SIZE = 16;
+export const ICON_SIZE = 16;
 
 export const navbarLinks = [
   { icon: <FiHome size={ICON_SIZE} />, color: "yellow", label: "Home" },
   { icon: <FiCircle size={ICON_SIZE} />, color: "cyan", label: "Free" },
   { icon: <FiBookOpen size={ICON_SIZE} />, color: "green", label: "About" },
   { icon: <FiSettings size={ICON_SIZE} />, color: "orange", label: "Settings" },
-];
-
-export const navbarLogout = [
-  { icon: <FiLogIn size={ICON_SIZE} />, color: "Indigo", label: "Login" },
 ];

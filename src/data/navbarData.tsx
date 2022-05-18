704d4cff201de34e886754819a68eb3947b272b2
min0k <mino.kim@minokim.com>
2022-05-17 23:01:22 -0700

    Change page names to lowercase

--- src/data/navbarData.tsx
+++ src/data/navbarData.tsx
@@ -3,8 +3,8 @@ import { FiHome, FiCircle, FiSettings, FiBookOpen } from "react-icons/fi";
 export const ICON_SIZE = 16;
 
 export const navbarLinks = [
-  { icon: <FiHome size={ICON_SIZE} />, color: "yellow", label: "Home" },
-  { icon: <FiCircle size={ICON_SIZE} />, color: "cyan", label: "Free" },
-  { icon: <FiBookOpen size={ICON_SIZE} />, color: "green", label: "About" },
-  { icon: <FiSettings size={ICON_SIZE} />, color: "orange", label: "Settings" },
+  { icon: <FiHome size={ICON_SIZE} />, color: "yellow", label: "home" },
+  { icon: <FiCircle size={ICON_SIZE} />, color: "cyan", label: "free" },
+  { icon: <FiBookOpen size={ICON_SIZE} />, color: "green", label: "about" },
+  { icon: <FiSettings size={ICON_SIZE} />, color: "orange", label: "settings" },
 ];

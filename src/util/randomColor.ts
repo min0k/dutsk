1a3e050e216d7632ac62b0c53ae20f177e2f6659
min0k <mino.kim@minokim.com>
2022-05-19 19:55:48 -0700

    Remove Red
    Red will be used as the main color for the Delete button. Update randomColor util to reflect
    number of available colors.

--- src/util/randomColor.ts
+++ src/util/randomColor.ts
@@ -1,5 +1,5 @@
 import { colors } from "../data/colors";
 
 export const randomColor = () => {
-  return colors[Math.floor(Math.random() * 12)];
+  return colors[Math.floor(Math.random() * 11)];
 };

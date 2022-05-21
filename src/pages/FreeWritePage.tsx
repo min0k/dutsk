9ad69e2d23eda43b9539b3bb1e415b2720898589
min0k <mino.kim@minokim.com>
2022-05-20 23:40:29 -0700

    Styling changes
    Use useRef hook to focus Textare on render, add unstyled variant to Textarea.

--- src/pages/FreeWritePage.tsx
+++ src/pages/FreeWritePage.tsx
@@ -1,9 +1,22 @@
 import { Textarea } from "@mantine/core";
+import { useEffect, useRef } from "react";
 
 export const FreeWritePage = () => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    ref.current!.focus();
+  }, []);
+
   return (
     <div>
-      <Textarea autosize radius="sm" placeholder="..." />
+      <Textarea
+        autosize
+        radius="sm"
+        placeholder="words words words..."
+        variant="unstyled"
+        ref={ref}
+      />
     </div>
   );
 };

06eae5e9a4ef64ed2f00790a905ac549ea09fe3d
min0k <mino.kim@minokim.com>
2022-05-20 20:01:12 -0700

    Add a period for branding

--- src/pageLayoutComponents/HeaderComponent.tsx
+++ src/pageLayoutComponents/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import { Mark, Text } from "@mantine/core";
 export const HeaderComponent = () => {
   return (
     <Text size="xl" color="blue" weight="600">
-      <Mark>Mindwrite</Mark>
+      <Mark>Mindwrite.</Mark>
     </Text>
   );
 };

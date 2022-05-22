029cbfdd5689e117eb4e26b0a40fe38e58af7ffb
min0k <mino.kim@minokim.com>
2022-05-22 10:32:48 -0700

    Ensure count is being incremented by one

--- src/api/updateCount.ts
+++ src/api/updateCount.ts
@@ -1,6 +1,5 @@
 export async function updateCount(id: string, count: number) {
-  const newCount = count++;
-  console.log(newCount);
+  const newCount = count + 1;
   try {
     let response = await fetch(
       `${process.env.REACT_APP_CLOUD_RUN_URL}/update-count?id=${id}&=count${newCount}`

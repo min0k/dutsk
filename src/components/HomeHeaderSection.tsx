e0780867d37bfc6695b5e2ec9545a496e4ee9301
min0k <mino.kim@minokim.com>
2022-05-22 08:42:46 -0700

    Add home description

--- src/components/HomeHeaderSection.tsx
+++ src/components/HomeHeaderSection.tsx
@@ -1,4 +1,5 @@
-import { Anchor, Box, Text } from "@mantine/core";
+import { Anchor, Box, List, Text, ThemeIcon, Title } from "@mantine/core";
+import { FiChevronRight, FiHome, FiMinus } from "react-icons/fi";
 export const HomeHeaderSection = () => {
   return (
     <div>
@@ -9,15 +10,30 @@ export const HomeHeaderSection = () => {
           borderRadius: theme.radius.md,
         })}
       >
-        <Text size="lg" pb="xl" pl="xl">
-          [Short description of what this is.]
-        </Text>
-        <Text size="lg" pb="sm" pl="xl" variant="text">
+        <Title pb="xl" pl="xl">
+          What is this?
+        </Title>
+        <List
+          spacing="xs"
+          size="md"
+          center
+          pb="xl"
+          icon={
+            <ThemeIcon color="blue" size={24} radius="xl">
+              <FiChevronRight />
+            </ThemeIcon>
+          }
+        >
+          <List.Item>Choose a question set and start writing.</List.Item>
+          <List.Item>Nothing you write is saved or seen.</List.Item>
+          <List.Item>Create your own questions for others to answer.</List.Item>
+        </List>
+        {/* <Text size="lg" pb="sm" pl="xl" variant="text">
           These questions were prepared by{" "}
           <Anchor weight="bold" href="https://mantine.dev/" target="_blank">
             [Person]
           </Anchor>
-        </Text>
+        </Text> */}
       </Box>
     </div>
   );

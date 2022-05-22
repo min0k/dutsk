38641171e52bf24e8919b5f6260766c7a53c3554
min0k <mino.kim@minokim.com>
2022-05-22 11:21:38 -0700

    Remove console.logs

--- src/api/updateCount.ts
+++ src/api/updateCount.ts
@@ -1,9 +1,5 @@
 export async function updateCount(id: string, count: number) {
-  console.log(count);
-
   const newCount = count + 1;
-  console.log(newCount);
-
   try {
     let response = await fetch(
       `${process.env.REACT_APP_CLOUD_RUN_URL}/update-count?id=${id}&count=${newCount}`

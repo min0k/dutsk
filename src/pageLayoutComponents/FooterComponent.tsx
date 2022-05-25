e68fbddc2e374d0b47b6ecf7f5629849b98f8a68
min0k <mino.kim@minokim.com>
2022-05-24 18:33:20 -0700

    refactor: create variable for portolio link

--- src/pageLayoutComponents/FooterComponent.tsx
+++ src/pageLayoutComponents/FooterComponent.tsx
@@ -1,10 +1,12 @@
 import { Anchor, Text } from "@mantine/core";
 
+const PORTFOLIO_URL = "https://www.minokim.com";
+
 export const FooterComponent = () => {
   return (
     <Text align="center" size="sm">
       Made by{" "}
-      <Anchor target="_blank" size="sm" href="https://www.minokim.com">
+      <Anchor target="_blank" size="sm" href={PORTFOLIO_URL}>
         Mino Kim
       </Anchor>
     </Text>
